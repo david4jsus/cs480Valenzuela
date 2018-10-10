4b1059f0619970edb0f83528259b644040ff5130
Hadi Rumjahn <hrumjahn@nevada.unr.edu>
2018-10-10 02:25:03 +0000

    Updated CmakeFile. Compiles in ECC

---
+++ PA6/build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/camera.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/camera.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/engine.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/engine.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/graphics.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/graphics.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/imgui.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/imgui.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/imgui_demo.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/imgui_demo.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/imgui_draw.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/imgui_draw.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/imgui_impl_opengl3.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/imgui_impl_opengl3.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/imgui_impl_sdl.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/imgui_impl_sdl.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/imgui_widgets.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/imgui_widgets.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/main.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/object.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/object.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/shader.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/shader.cpp.o"
+  "/home/hrumjahn/cs480Valenzuela/PA6/src/window.cpp" "/home/hrumjahn/cs480Valenzuela/PA6/build/CMakeFiles/Tutorial.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNIX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/include"
+  "../include"
+  "/usr/include/SDL2"
+  "/usr/include/ImageMagick-6"
+  "/usr/include/x86_64-linux-gnu/ImageMagick-6"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PA6/include/object.h
@@ -0,0 +1,88 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+#include <assimp/Importer.hpp> //includes the importer, which is used to read our obj file
+#include <assimp/scene.h> //includes the aiScene object
+#include <assimp/postprocess.h> //includes the postprocessing variables for the importer
+#include <assimp/color4.h> //includes the aiColor4 object, which is used to handle the colors from the mesh 
+
+#include <Magick++.h>
+
+class Object
+{
+  public:
+    Object();
+    Object(std::string filePath, Object* objParent, float objOrbitRadius, float objOrbitMultiplier,
+      float objRotateMultiplier, float objSize);
+    ~Object();
+    void createObject();
+    void Update(unsigned int dt);
+    void Render();
+
+    glm::mat4 GetModel();
+    glm::mat4 GetModelForChild();
+    
+    // To reverse direction of rotation
+    void reverseDirection();
+    // To make direction counter-clockwise
+    void makeDirectionCounter();
+    // To make direction clockwise
+    void makeDirectionClockwise();
+    // Toggle cube rotating (self-centered)
+    void toggleRotation();
+    // Toggle cube orbiting
+    void toggleOrbit();
+    
+    // Check if reverse direction for orbit
+    bool isDirectionReversed();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+    
+    // If the object has a child, this is the matrix it should read as its center
+    glm::mat4 modelForChild;
+
+    float angle;
+    float rotAngle;
+    
+    // To reverse direction of rotation based on keyboard input
+    bool directionReversed;
+    // To rotate in place
+    bool rotating;
+    // To orbit around
+    bool orbiting;
+    
+    // Planet/moon properties
+    Object* parent;
+    float orbitRadius;
+    float orbitSpeedMultiplier;
+    float rotateSpeedMultiplier;
+    float size;
+    
+    // Object loading
+    std::string objFilePath;
+    bool correctModelLoad;
+    std::vector<Vertex> myVertices;
+    std::vector<unsigned int> myIndices;
+    bool loadOBJ(std::string path, std::vector<Vertex> &out_vertices, std::vector<unsigned int> &out_indices);
+    void LoadTexture(std::string
+
+
+	// Assimp object loader
+	Assimp::Importer importer;
+	const aiScene *scene;
+	std::vector<aiMesh*> meshes;
+	
+	
+	Magick::Image im;
+	
+};
+
+#endif /* OBJECT_H */
