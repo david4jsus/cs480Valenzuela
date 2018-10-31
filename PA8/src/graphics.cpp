e3e8961e511d27b3a240a9a4dfede3eaf6c079b7
Jay Woo <jjwoo95@gmail.com>
2018-10-31 02:19:47 +0000

    Combined PA8 with PA7.

---
+++ PA8/src/graphics.cpp
@@ -0,0 +1,239 @@
+#include "graphics.h"
+
+Graphics::Graphics()
+{
+	broadphase = NULL;
+	collisionConfig = NULL;
+	dispatcher = NULL;
+	solver = NULL;
+	dynamicsWorld = NULL;
+}
+
+Graphics::~Graphics()
+{
+    if( dynamicsWorld != NULL )
+    {
+        delete dynamicsWorld;
+
+        dynamicsWorld = NULL;
+    }
+
+    if( solver != NULL )
+    {
+        delete solver;
+
+        solver = NULL;
+    }
+
+    if( dispatcher != NULL )
+    {
+        delete dispatcher;
+
+        dispatcher = NULL;
+    }
+
+    if( collisionConfig != NULL )
+    {
+        delete collisionConfig;
+
+        collisionConfig = NULL;
+    }
+
+    if( broadphase != NULL )
+    {
+        delete broadphase;
+
+        broadphase = NULL;
+    }
+}
+
+bool Graphics::Initialize(int width, int height, std::string file)
+{
+  // Used for the linux OS
+  #if !defined(__APPLE__) && !defined(MACOSX)
+    // cout << glewGetString(GLEW_VERSION) << endl;
+    glewExperimental = GL_TRUE;
+
+    auto status = glewInit();
+
+    // This is here to grab the error that comes from glew init.
+    // This error is an GL_INVALID_ENUM that has no effects on the performance
+    glGetError();
+
+    //Check for error
+    if (status != GLEW_OK)
+    {
+      std::cerr << "GLEW Error: " << glewGetErrorString(status) << "\n";
+      return false;
+    }
+  #endif
+
+  // For OpenGL 3
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // Init Camera
+  m_camera = new Camera();
+  if(!m_camera->Initialize(width, height))
+  {
+    printf("Camera Failed to Initialize\n");
+    return false;
+  }
+
+  // Create planets
+  Object* Skybox = new Object("Skybox.obj", 0, 0.0f, 0.0f, 0.0f, 25.0f);// file path, parent, orbit radius size, orbit radius speed, local rotation speed, object size
+  Object* buddha = new Object("buddha.obj", 0, 0.0f, 0.0f, 0.0f, 25.0f); // file path, parent, orbit radius size, orbit radius speed, local rotation speed, object size
+
+  // Waiting Song while the planets load
+  gameSound.LoadSound("../assets/jeopardy.wav");
+  gameSound.PlaySound();
+  
+  // push planets onto list
+  m_cubes.push_back(Skybox);
+  m_cubes.push_back(buddha);
+
+  // Set up the shaders
+  m_shader = new Shader();
+  if(!m_shader->Initialize())
+  {
+    printf("Shader Failed to Initialize\n");
+    return false;
+  }
+
+  // Add the vertex shader
+  if(!m_shader->AddShader(GL_VERTEX_SHADER))
+  {
+    printf("Vertex Shader failed to Initialize\n");
+    return false;
+  }
+
+  // Add the fragment shader
+  if(!m_shader->AddShader(GL_FRAGMENT_SHADER))
+  {
+    printf("Fragment Shader failed to Initialize\n");
+    return false;
+  }
+
+  // Connect the program
+  if(!m_shader->Finalize())
+  {
+    printf("Program to Finalize\n");
+    return false;
+  }
+
+  // Locate the projection matrix in the shader
+  m_projectionMatrix = m_shader->GetUniformLocation("projectionMatrix");
+  if (m_projectionMatrix == INVALID_UNIFORM_LOCATION) 
+  {
+    printf("m_projectionMatrix not found\n");
+    return false;
+  }
+
+  // Locate the view matrix in the shader
+  m_viewMatrix = m_shader->GetUniformLocation("viewMatrix");
+  if (m_viewMatrix == INVALID_UNIFORM_LOCATION) 
+  {
+    printf("m_viewMatrix not found\n");
+    return false;
+  }
+
+  // Locate the model matrix in the shader
+  m_modelMatrix = m_shader->GetUniformLocation("modelMatrix");
+  if (m_modelMatrix == INVALID_UNIFORM_LOCATION) 
+  {
+    printf("m_modelMatrix not found\n");
+    return false;
+  }
+
+  //enable depth testing
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+
+  return true;
+}
+
+void Graphics::Update(unsigned int dt)
+{
+  // Update the objects
+  for(unsigned int i = 0; i < m_cubes.size(); i++)
+  {
+    m_cubes[i]->Update(dt);
+  }
+}
+
+Camera* Graphics::getCamera()
+{
+   return m_camera;
+}
+
+Object* Graphics::GetObject(int index)
+{
+  return m_cubes[index];
+}
+
+int Graphics::numberOfCubes()
+{
+  return m_cubes.size();
+}
+
+void Graphics::Render()
+{
+  //clear the screen
+  glClearColor(0.0, 0.0, 0.0, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Start the correct program
+  m_shader->Enable();
+
+  // Send in the projection and view to the shader
+  glUniformMatrix4fv(m_projectionMatrix, 1, GL_FALSE, glm::value_ptr(m_camera->GetProjection())); 
+  glUniformMatrix4fv(m_viewMatrix, 1, GL_FALSE, glm::value_ptr(m_camera->GetView())); 
+
+  // Render the objects
+  for(unsigned int i = 0; i < m_cubes.size(); i++)
+  {
+    glUniformMatrix4fv(m_modelMatrix, 1, GL_FALSE, glm::value_ptr(m_cubes[i]->GetModel()));
+    m_cubes[i]->Render();
+  }
+
+  // Get any errors from OpenGL
+  auto error = glGetError();
+  if ( error != GL_NO_ERROR )
+  {
+    string val = ErrorString( error );
+    std::cout<< "Error initializing OpenGL! " << error << ", " << val << std::endl;
+  }
+}
+
+std::string Graphics::ErrorString(GLenum error)
+{
+  if(error == GL_INVALID_ENUM)
+  {
+    return "GL_INVALID_ENUM: An unacceptable value is specified for an enumerated argument.";
+  }
+
+  else if(error == GL_INVALID_VALUE)
+  {
+    return "GL_INVALID_VALUE: A numeric argument is out of range.";
+  }
+
+  else if(error == GL_INVALID_OPERATION)
+  {
+    return "GL_INVALID_OPERATION: The specified operation is not allowed in the current state.";
+  }
+
+  else if(error == GL_INVALID_FRAMEBUFFER_OPERATION)
+  {
+    return "GL_INVALID_FRAMEBUFFER_OPERATION: The framebuffer object is not complete.";
+  }
+
+  else if(error == GL_OUT_OF_MEMORY)
+  {
+    return "GL_OUT_OF_MEMORY: There is not enough memory left to execute the command.";
+  }
+  else
+  {
+    return "None";
+  }
+}
---
+++ PA8/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "engine.h"
+
+
+int main(int argc, char **argv)
+{
+  // Start an engine and run it then cleanup after
+  std::string model("");
+  if (argc > 1)
+  {
+    model = argv[1];
+  }
+  
+  Engine *engine = new Engine("CS 480 - Team Specular Bois - PA7", 1680, 1050, model);
+  
+  if(!engine->Initialize())
+  {
+    printf("The engine failed to start.\n");
+    delete engine;
+    engine = NULL;
+    return 1;
+  }
+  
+  engine->Run();
+  delete engine;
+  engine = NULL;
+  return 0;
+}
