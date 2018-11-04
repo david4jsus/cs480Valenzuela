82b10d0810e8a8ffc43e2c8be2102701412be5e9
Hadi Rumjahn <hrumjahn@nevada.unr.edu>
2018-11-04 23:52:22 +0000

    Adding Shaders and Shader class now takes a filename

--- PA9/src/graphics.cpp
+++ PA9/src/graphics.cpp
@@ -121,14 +121,14 @@ bool Graphics::Initialize(int width, int height, std::string file)
   }
 
   // Add the vertex shader
-  if(!m_shader->AddShader(GL_VERTEX_SHADER))
+  if(!m_shader->AddShader(GL_VERTEX_SHADER, "../assets/shaders/vertex.shader"))
   {
     printf("Vertex Shader failed to Initialize\n");
     return false;
   }
 
   // Add the fragment shader
-  if(!m_shader->AddShader(GL_FRAGMENT_SHADER))
+  if(!m_shader->AddShader(GL_FRAGMENT_SHADER, "../assets/shaders/fragment.shader"))
   {
     printf("Fragment Shader failed to Initialize\n");
     return false;
